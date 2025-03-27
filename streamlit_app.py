4275174138b05a8f178e59bad5b6760b5fd90e74
yt-koike <y.koike.2001code@gmail.com>
2025-03-27 21:49:13 +0900

    画像を横に並べるよう変更

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 from sim import ImageSimularity
 
-st.title("画像類似度比較アプリ")
+st.title("画像類似度比較 Web アプリ")
 
 filepath1 = st.file_uploader('画像をアップロードしてください.', type=['jpg', 'jpeg', 'png'],key=1)
 filepath2 = st.file_uploader('画像をアップロードしてください.', type=['jpg', 'jpeg', 'png'],key=2)
 
+st.write(st.get_container_width)
 if filepath1 is not None and filepath2 is not None:
-    st.image(filepath1)
-    st.image(filepath2)
+    st.image([filepath1,filepath2], width=300)
    
     imSim = ImageSimularity()
-    path_img0 = "a.png"
     img0 =imSim.loadImage(filepath1)
     img1 =imSim.loadImage(filepath2)
 
